2c279c8ae0af284b64c45a5d2ea270a631a52952
fractalbarista <fractalbarista@gmail.com>
2026-07-09 10:48:38 +0900

    Axios를 의존성 패키지에 추가 여행계획생성 버튼을 누르면 통신이 이루어지도록 핸들러 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import {Box, Button, Container, Stack,} from "@mui/material";
 import {DestinationKeywordInputCard} from "./components/DestinationKeywordInputCard.jsx";
 import {KoreanLocaleDatePicker} from "./components/KoreanLocaleDatePicker.jsx";
 import {TravelAreaSelect} from "./components/TravelAreaSelect.jsx";
+import axios from "axios";
+
+const API_URL = "http://localhost:8080";
 
 function App() {
   const [area, setArea] = useState(null);
@@ -28,6 +31,32 @@ function App() {
     }
   ]);
 
+  async function handleGenerateButtonClick() {
+    await axios.create()
+        .post(
+            API_URL,
+            {
+              area: area,
+              startDate: startDate,
+              endDate: endDate,
+              destinations: destinations
+            },
+            {}
+        )
+        .then(response => {
+          if (response.status !== 200) {
+            return;
+          }
+
+          console.log("결과페이지로 이동");
+          console.log(response.data);
+
+        })
+        .catch(error => {
+          console.log(error);
+        });
+  }
+
   return (
       <Container maxWidth={"sm"}>
         <Box>
@@ -41,7 +70,7 @@ function App() {
               ))
             }
             <Button variant={"outlined"}>여행지 추가</Button>
-            <Button variant={"contained"} sx={{display: "block"}}>여행 계획 생성</Button>
+            <Button variant={"contained"} onClick={handleGenerateButtonClick} sx={{display: "block"}}>여행 계획 생성</Button>
           </Stack>
         </Box>
       </Container>
