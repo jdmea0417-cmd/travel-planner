fd6ff5d75aad7a8aba6b9ab7a691d9a0c2604e03
fractalbarista <fractalbarista@gmail.com>
2026-07-10 12:50:26 +0900

    MainPage와 ResultPage 최상위 Container가 뷰포트 높이 전체를 차지하도록 수정

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,38 +6,63 @@ import {
   Container,
   Box,
   Card,
-  CardContent,
+  CardContent, Divider,
 } from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+
+  function handleSignupButtonClick() {
+    navigate("/signup");
+  }
+
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ mt: 5 }}>
-        <Card>
+      <Container
+          maxWidth="xs"
+          sx={{
+            display: "flex",
+            height: "100vh",
+            alignItems: "center"
+          }}
+      >
+        <Card sx={{ width: "100%" }}>
           <CardContent>
             <Stack spacing={2}>
-              <Typography variant="h5">
+              <Typography
+                  variant="h5"
+              >
                 로그인
               </Typography>
 
               <TextField
-                label="아이디"
-                variant="outlined"
+                  label="아이디"
+                  variant="outlined"
               />
 
               <TextField
-                label="비밀번호"
-                type="password"
-                variant="outlined"
+                  label="비밀번호"
+                  type="password"
+                  variant="outlined"
               />
 
-              <Button variant="contained">
+              <Button
+                  variant="contained"
+              >
                 로그인 하기
+              </Button>
+
+              <Divider variant="inset"></Divider>
+
+              <Button
+                  variant="contained"
+                  onClick={handleSignupButtonClick}
+              >
+                회원가입 하기
               </Button>
             </Stack>
           </CardContent>
         </Card>
-      </Box>
-    </Container>
+      </Container>
   );
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -8,53 +8,46 @@ import {
   Button,
   Typography,
 } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
-  const navigate = useNavigate();
-
-  function handleLoginButtonClick() {
-    navigate("/login");
-  }
-
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ mt: 5 }}>
-        <Card>
+      <Container
+          maxWidth="xs"
+          sx={{
+            display: "flex",
+            height: "100vh",
+            alignItems: "center"
+          }}
+      >
+        <Card sx={{ width: "100%" }}>
           <CardContent>
             <Stack spacing={2}>
-              <Typography variant="h5">회원가입</Typography>
+              <Typography variant="h5">
+                회원가입
+              </Typography>
 
               <TextField
-                label="이름"
-                variant="outlined"
+                  label="이름"
+                  variant="outlined"
               />
 
               <TextField
-                label="아이디"
-                variant="outlined"
+                  label="아이디"
+                  variant="outlined"
               />
 
               <TextField
-                label="비밀번호"
-                type="password"
-                variant="outlined"
+                  label="비밀번호"
+                  type="password"
+                  variant="outlined"
               />
 
               <Button variant="contained">
                 가입하기
               </Button>
-
-              <Button
-                variant="outlined"
-                onClick={handleLoginButtonClick}
-              >
-                로그인으로 이동
-              </Button>
             </Stack>
           </CardContent>
         </Card>
-      </Box>
-    </Container>
+      </Container>
   );
 }
