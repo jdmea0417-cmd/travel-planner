515e4adbd6731878b178ebc216c353a9ca08cfcd
fractalbarista <fractalbarista@gmail.com>
2026-07-10 10:28:26 +0900

    `CssBaseLine` 컴포넌트를 추가하여 body의 마진을 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {MainPage} from "./pages/MainPage.jsx";
 import {ResultPage} from "./pages/ResultPage.jsx";
+import {CssBaseline} from "@mui/material";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage/>}/>
-          <Route path="/result" element={<ResultPage/>}/>
-        </Routes>
-      </BrowserRouter>
+      <>
+        <CssBaseline/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage/>}/>
+            <Route path="/result" element={<ResultPage/>}/>
+          </Routes>
+        </BrowserRouter>
+      </>
   );
 }
 
