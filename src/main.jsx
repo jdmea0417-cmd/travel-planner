be211911275fcd8a94b4fe09e61a9b6f0ca12399
fractalbarista <fractalbarista@gmail.com>
2026-07-11 11:28:38 +0900

    DarkModeContext 컴포넌트 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,14 @@ import App from './App.jsx'
 import {CssBaseline, ThemeProvider} from "@mui/material";
 import {theme} from "./theme.js";
 import {AccessTokenContextProvider} from "./contexts/AccessTokenContext.jsx";
-import {DarkModeContextProvider} from "./contexts/DarkModeContext.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
       <ThemeProvider theme={theme}>
         <CssBaseline/>
-        <DarkModeContextProvider>
           <AccessTokenContextProvider>
             <App/>
           </AccessTokenContextProvider>
-        </DarkModeContextProvider>
       </ThemeProvider>
     </StrictMode>,
 )
