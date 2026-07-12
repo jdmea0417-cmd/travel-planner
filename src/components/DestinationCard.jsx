82d1644cec6ce9826046c7a3465a02eda8081e6f
fractalbarista <fractalbarista@gmail.com>
2026-07-12 12:09:41 +0900

    CardHeader와 CardContent 사이의 공백을 제거

--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -37,7 +37,7 @@ export const DestinationCard = ({ keywords, onDelete, onRemoveKeyword, onAddKeyw
               </IconButton>
             }
         ></CardHeader>
-        <CardContent>
+        <CardContent sx={{paddingTop: 0}}>
           <Stack spacing={2}>
             <Stack direction="row" spacing={1} sx={{ overflowX: "auto", height: "32px" }}>
               {
