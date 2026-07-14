ab2e5d06bb5cf38b50313d4abeaa5c094c6fc206
Junwoo Shin <wnsdn020707@gmail.com>
2026-07-14 14:25:55 +0900

    refactor: remove duplicated edit state

--- src/pages/TimelinePage.jsx
+++ src/pages/TimelinePage.jsx
@@ -33,10 +33,6 @@ export const TimelinePage = () => {
 
   const [ editingIndex, setEditingIndex ] = useState(null);
 
-  const [editedPlace, setEditedPlace] = useState("");
-  const [editedDate, setEditedDate] = useState("");
-  const [editedTime, setEditedTime] = useState("");
-
   function isLoggedIn() {
     return true;
     return accessToken !== null;
@@ -61,10 +57,6 @@ export const TimelinePage = () => {
                 <IconButton
                     onClick={() => {
                       setEditingIndex(index);
-
-                      setEditedPlace(destination.place);
-                      setEditedDate(destination.date);
-                      setEditedTime(destination.time);
                     }}
                 >
                     <EditIcon/>
